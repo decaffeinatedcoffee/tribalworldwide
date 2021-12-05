3a7b98c4b942e5ced96cb12012ac9d7bb6095cf3
decaffeinatedcoffee <77131841+decaffeinatedcoffee@users.noreply.github.com>
2021-12-05 17:14:27 -0300

    Create scriptfinal.js

---
+++ assets/scriptfinal.js
@@ -0,0 +1,18 @@
+countdown();
+function countdown() {
+let data = new Date();
+data.toLocaleString('en-US', { timeZone: 'America/Sao_Paulo' });
+var atual = new Date().getTime();
+var end = new Date('Dec 25, 2021 00:00:00').getTime();
+var restante = end - atual;
+var dias = Math.floor(restante / (1000 * 60 * 60 * 24));
+var horas = Math.floor((restante % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+var minutos = Math.floor((restante % (1000 * 60 * 60)) / (1000 * 60));
+var segundos = Math.floor((restante % (1000 * 60)) / 1000);
+if(dias <= 0 && horas <= 0 && minutos <= 0 && segundos <= 0){
+    location.replace('https://tribalworldwide.vercel.app/') 
+}
+else{
+    setTimeout(countdown, 1000);
+}
+}
